2bda9663559857053dac7cd690cbae19e8f6a870
facerbot <83787101+facerbot@users.noreply.github.com>
2022-04-22 23:19:45 +0100

    New website here are is.
    A lot of changes

---
+++ README.md
@@ -0,0 +1,22 @@
+FACER 2022
+========================
+
+[![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Ffacerbot.github.io&count_bg=%2330665B&title_bg=%23555555&icon=letsencrypt.svg&icon_color=%23FFFFFF&title=VISITS&edge_flat=false)](https://hits.seeyoufarm.com)
+
+
+## Get in touch with us 🙋
+
+If you need any help, want to report a bug or new idea please join [SUPPORT SERVER](https://discord.gg/5UK939STjX) on discord.
+
+## We ❤️
+
+We are a very small team that works on the bot.
+We really appreciate every person who added/uses the bot and submits new ideas/bugs.
+
+
+### How to add bot 🪟
+
+1. Get the [link to invite](https://discord.com/api/oauth2/authorize?client_id=870438284927565885&permissions=8&scope=bot).
+2. Choose the server you want your bot in.
+3. Have fun.
+
---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if(this.scrollY > 20)
+      $(".navbar").addClass("sticky");
+    else
+      $(".navbar").removeClass("sticky");
+  });
+
+  $('.menu-toggler').click(function(){
+    $(this).toggleClass("active");
+    $(".navbar-menu").toggleClass("active");
+  });
+});
+
+
+$(window).on("load resize ", function() {
+  var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+  $('.tbl-header').css({'padding-right':scrollWidth});
+}).resize();
+
+
+const counters = document.querySelectorAll('.count');
+const speed = 200;
+
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = parseInt(counter.getAttribute('data-target'));
+    const count = parseInt(counter.innerText);
+    const increment = Math.trunc(target / speed);
+
+    if (count < target) {
+      counter.innerText = count + increment;
+      setTimeout(updateCount, 1);
+    } else {
+      counter.innerText = target;
+    }
+  };
+  updateCount();
+});
